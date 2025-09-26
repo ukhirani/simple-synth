724be277067ce7321b6c65af2ea4d17b6b9de587
Umang Hirani <umanghirani.exe@gmail.com>
2025-09-26 19:52:28 +0530

    first four files of CP

---
+++ Source/BypassComponent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ukhirani on 9/26/25.
+//
+
+#include "BypassComponent.h"
---
+++ Source/BypassComponent.h
@@ -0,0 +1,13 @@
+//
+// Created by ukhirani on 9/26/25.
+//
+
+#ifndef SIMPLESYNTH_BYPASSCOMPONENT_H
+#define SIMPLESYNTH_BYPASSCOMPONENT_H
+
+
+class BypassComponent {
+};
+
+
+#endif //SIMPLESYNTH_BYPASSCOMPONENT_H
